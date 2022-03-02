76df69efd7f5ccca0734ecea6e7b9503dfa8c101
tomihasen <thasenhundl@gmail.com>
2022-03-01 21:05:56 -0300

    agrego objetos y arrays

--- js/index.js
+++ js/index.js
@@ -1,37 +1,27 @@
-let nombreCombo1 = "hamburguesa simple";
-let precioCombo1 = 650;
 
-let nombreCombo2 = "hamburguesa doble";
-let precioCombo2 = 750;
+function Combo (nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+}
 
-let nombreCombo3 = "hamburguesa triple";
-let precioCombo3 = 850;
+const combo1 = new Combo ("hamburguesa simple", 650)
+const combo2 = new Combo ("hamburguesa doble", 750)
+const combo3 = new Combo ("hamburguesa triple", 850)
+const combo4 = new Combo ("hamburguesa clasica", 650)
+const combo5 = new Combo ("hamburguesa de pollo", 700)
+const combo6 = new Combo ("hamburguesa vegetariana", 600)
 
-let nombreCombo4 = "hamburguesa clasica";
-let precioCombo4 = 650;
+const listaDeCombos = [combo1, combo2, combo3, combo4, combo5, combo6];
 
-let nombreCombo5 = "hamburguesa de pollo";
-let precioCombo5 = 700;
-
-let nombreCombo6 = "hamburguesa vegetariana";
-let precioCombo6 = 600;
-
-
-let unicaCompra;
 let cantidadAComprar;
 let totalVenta = 0;
 let combosAComprar = parseInt(prompt("Ingrese la cantidad de combos distintos que desea comprar:"));
 
-function solicitarPedido (combo, nombre, cantidad, total, precio, unica) {
+function solicitarPedido (combo, nombre, cantidad, total, precio) {
 
     if(combo == nombre){
-
         cantidad = parseInt(prompt("¿Cuántas hamburguesas del combo elegido desea comprar?"));
         total += (cantidad * precio);
-        unica = prompt("¿Será esta su compra final?").toLowerCase();
-        if(unica == "si"){
-            return;
-        }
     }
 }
 
@@ -41,21 +31,21 @@ function productoNoExistente () {
 
 for (let i = 0; i < combosAComprar; i++){
 
-    let comboPedido = prompt("Ingrese el nombre del combo que desea comprar \n1)Hamburguesa simple \n2)Hamburguesa doble \n3)Hamburguesa triple \n4)Hamburguesa clasica \n5)Hamburguesa de pollo \n6)Hamburguesa vegetariana").toLowerCase();
-
-    solicitarPedido (comboPedido, nombreCombo1, cantidadAComprar, totalVenta, precioCombo1, unicaCompra);
-
-    solicitarPedido (comboPedido, nombreCombo2, cantidadAComprar, totalVenta, precioCombo2, unicaCompra);
-
-    solicitarPedido (comboPedido, nombreCombo3, cantidadAComprar, totalVenta, precioCombo3, unicaCompra);
-
-    solicitarPedido (comboPedido, nombreCombo4, cantidadAComprar, totalVenta, precioCombo4, unicaCompra);
-
-    solicitarPedido (comboPedido, nombreCombo5, cantidadAComprar, totalVenta, precioCombo5, unicaCompra);
-
-    solicitarPedido (comboPedido, nombreCombo6, cantidadAComprar, totalVenta, precioCombo6, unicaCompra);
+    let comboPedido = prompt("Ingrese el nombre del combo que desea comprar \n 1) Hamburguesa simple: $650 \n 2) Hamburguesa doble: $750\n 3) Hamburguesa triple: $850 \n 4) Hamburguesa clasica: $650 \n 5) Hamburguesa de pollo: $700 \n 6) Hamburguesa vegetariana: $600").toLowerCase();
     
-    if ((comboPedido != nombreCombo1.toLowerCase()) && (comboPedido != nombreCombo2.toLowerCase()) && (comboPedido != nombreCombo3.toLowerCase()) && (comboPedido != nombreCombo4.toLowerCase()) && (comboPedido != nombreCombo5.toLowerCase()) && (comboPedido != nombreCombo6.toLowerCase())) {
+    solicitarPedido (comboPedido, listaDeCombos[0].nombre, cantidadAComprar, totalVenta, listaDeCombos[0].precio);
+
+    solicitarPedido (comboPedido, listaDeCombos[1].nombre, cantidadAComprar, totalVenta, listaDeCombos[1].precio);
+
+    solicitarPedido (comboPedido, listaDeCombos[2].nombre, cantidadAComprar, totalVenta, listaDeCombos[2].precio);
+
+    solicitarPedido (comboPedido, listaDeCombos[3].nombre, cantidadAComprar, totalVenta, listaDeCombos[3].precio);
+
+    solicitarPedido (comboPedido, listaDeCombos[4].nombre, cantidadAComprar, totalVenta, listaDeCombos[4].precio);
+
+    solicitarPedido (comboPedido, listaDeCombos[5], cantidadAComprar, totalVenta, listaDeCombos[5].precio);
+    
+    if ((comboPedido != combo1.nombre) && (comboPedido != combo2.nombre) && (comboPedido != combo3.nombre) && (comboPedido != combo4.nombre) && (comboPedido != combo5.nombre) && (comboPedido != combo6.nombre)) {
         productoNoExistente ()
     }
 
